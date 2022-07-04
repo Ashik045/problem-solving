4200dc269ae19948ee91fb087eeee083c9c4286a
Ashik045 <ashikurislam045@gmail.com>
2022-07-04 15:19:49 +0600

    first commit

---
+++ easy-problems/index.js
@@ -0,0 +1,12 @@
+// Two sum
+var twoSum = function(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if ((nums[i] + nums[j] ) === target) {
+                return [i, j];
+            }
+        }
+    }
+    return false;
+};
+console.log(twoSum([4, 3, 2, 7, 1, 5], 100));
